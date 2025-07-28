734f30ceb5f98a1a34c1b2af9f7951b97909fb0e
Revathimerugu30 <revathimerugu30jits@gmail.com>
2025-07-28 14:49:52 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Iam a Student");
+        System.out.println("Hello,from Jenkins");
     }
 }
